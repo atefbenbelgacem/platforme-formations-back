41f13e332d691618ce4b6c7ad12b5d1270439352
Atef Ben Belgacem <atef.ben-belgacem@proxym-it.com>
2020-10-01 16:37:18 +0100

    copied e2e test file

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,11 +2,12 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { CreateUserDto } from 'src/components/users/createUser.dto';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -15,10 +16,33 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('should (GET) Hello world', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
       .expect('Hello World!');
+  });
+
+
+  let user: CreateUserDto = {
+    name: "test",
+    lastName: "test",
+    email: "test@test.com",
+    password: "test123",
+    pole: "abc",
+    roleName: "Colab"
+  }
+
+  test('should (Post) a user', () => {
+    return request(app.getHttpServer())
+      .post('api/users')
+      .send(user)
+      .set('Accept', 'application/json')
+      .expect(201)
+      .expect('Content-Type', /json/)
+  });
+
+  afterAll(async () => {
+    await app.close()
   });
 });
